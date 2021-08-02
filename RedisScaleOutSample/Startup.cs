99145927b897b3aa0fda149352b681826d7c8258
Dante Alferez Condori <dalcon@hotmail.com>
2021-08-02 15:43:20 -0500

    Initial commit, libraries updates

---
+++ RedisScaleOutSample/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+
+namespace RedisScaleOutSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //GlobalHost.DependencyResolver.UseRedis("10.0.2.2", 16379, string.Empty, "myApp");
+            GlobalHost.DependencyResolver.UseStackExchangeRedis("10.0.2.2", 16379, string.Empty, "myApp");
+            app.MapSignalR();
+        }
+    }
+}
